befbf4244ac8a52034d48a089605210e6ea1a1a8
tarcisiomail <91227233+tarcisiomail@users.noreply.github.com>
2022-06-17 19:13:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,46 +11,35 @@ st.set_page_config(page_title='Alexandria',
                    initial_sidebar_state='expanded',
                    menu_items=None)
 
-def alexandria():
+
+
+options = st.sidebar.radio("SELECIONE UMA OPÇÃO: ",("Alexandria - Início",
+                                                    "Novo Empréstimo",
+                                                    "Finalizar Empréstimo",
+                                                    "Cadastrar Livro",
+                                                    "Cadastrar Aluno",
+                                                    "Tabelas do Sistema"),index=0)
+if options == "Alexandria - Início":
     st.title("SISTEMA DE GESTÃO DE SALA DE LEITURA ESCOLAR")
     st.success("INSTRUÇÔES:")
-    st.write("As seguintes opções estão disponíveis no menu à esquerda:")
+    st.subheader("As seguintes opções estão disponíveis no menu lateral à esquerda:")
     st.write("*Novo Empréstimo")
     st.write("*Finalizar Empréstimo")
     st.write("*Cadastrar Livro")
     st.write("*Cadastrar Aluno")
     st.write("*Tabelas do Sistema")
 
-
-def novo_emprestimo():
-    st.markdown("Novo Empréstimo")
+if options == "Novo Empréstimo":
     tela_novo_emprestimo()
 
-def livro():
-    st.markdown("Cadastrar Livro")
-    tela_cadastro_livro()
-
-def aluno():
-    st.markdown("Cadastrar Aluno")
-    tela_cadastro_aluno()
-
-def tabelas():
-    tela_consulta_tabelas()
-
-def finalizar_emprestimo():
-    st.markdown("Finalizar Empréstimo")
+if options == "Finalizar Empréstimo":
     tela_finalizar_emprestimo()
 
+if options == "Cadastrar Livro":
+    tela_cadastro_livro()
 
-page_names_to_funcs = {
-    "Alexandria - Início": alexandria,
-    "Novo Empréstimo": novo_emprestimo,
-    "Finalizar Emprestimo": finalizar_emprestimo,
-    "Cadastrar Livro": livro,
-    "Cadastrar Aluno": aluno,
-    "Tabelas do Sistema": tabelas,
+if options == "Cadastrar Aluno":
+    tela_cadastro_aluno()
 
-}
-
-page = st.sidebar.selectbox("Selecione uma opção", page_names_to_funcs.keys())
-page_names_to_funcs[page]()
+if options == "Tabelas do Sistema":
+    tela_consulta_tabelas()
